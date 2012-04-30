68f7e1f39c6cc7430d4fb4f662367e41851640f2
Tero Auvinen <tero@seravo.fi>
2012-04-30 08:19:16 +0300

    Filter by service name_fi, not type.

--- js/libs/jquery.facetview.js
+++ js/libs/jquery.facetview.js
@@ -160,7 +160,8 @@
 		{'field': 'consortium', 'size': 100, 'order':'term', 'display': 'Library group'},
 		{'field': 'organisation_type', 'display': 'Type'},
 		{'field': 'branch_type', 'display': 'Branch type'},
-		{'field': 'services.type', 'display': 'Services'},
+		{'field': 'services.name_fi', 'display': 'Services'},
+		{'field': 'accessibility.accessible_entry', 'display': 'Accessibility'},
 		{'field': 'contact.street_address.municipality_fi', 'display': 'City'}
 		],
             "addremovefacets": false,
