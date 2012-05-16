bf8d0fc3e79f83893893103f8ad3bd803a902cc3
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2012-05-16 10:41:36 +0300

    Added delay before proxy, so that ES is more likely to be ready.

--- start-all-servers.sh
+++ start-all-servers.sh
@@ -8,8 +8,12 @@ set -e
 set -x
 
 gnome-terminal -x elasticsearch-0.18.7/bin/elasticsearch -f
+sleep 8 # wait for ES to come up
 gnome-terminal -x node proxy.js
 gnome-terminal -x node server.js
 
-echo "All servers running in their own terminals."
-echo "Open in a browser http://localhost:8080/"
+# Build static
+lessc style.less > style.css
+h5bp
+
+echo "All servers running in their own terminals. Open in a browser http://localhost:8080/"
