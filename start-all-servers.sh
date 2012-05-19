31298fd1b3adb5dbcedb0b48d92bb896ef0f3c88
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2012-05-19 15:53:01 +0300

    Starting server now after build, won't work otherwise..

--- start-all-servers.sh
+++ start-all-servers.sh
@@ -7,17 +7,17 @@ set -e
 # print commands before execution
 set -x
 
-gnome-terminal -x elasticsearch-0.18.7/bin/elasticsearch -f
-sleep 8 # wait for ES to come up
-gnome-terminal -x node proxy.js
-gnome-terminal -x node server.js
-
 # Build static
 cd css
 lessc style.less > style.css
 cd ..
-
 h5bp
+
+# start servers
+gnome-terminal -x elasticsearch-0.18.7/bin/elasticsearch -f
+sleep 8 # wait for ES to come up
+gnome-terminal -x node proxy.js
+gnome-terminal -x node server.js
 
 echo "All servers running in their own terminals. Open in a browser http://localhost:8080/"
 
