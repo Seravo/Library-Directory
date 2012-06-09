05c138b913fc17fbce9eaec82509fa39d9fd886d
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2012-06-09 23:28:48 +0300

    Made script executable, put cron line and added logging to syslog with logger-command.

--- bin/update_es_data.sh
+++ bin/update_es_data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Server specific script to update ES data folder
-# Run nightly
+# Run nightly with crontab line like
+# 0 5 * * 1 /var/www/libdir/bin/update_es_data.sh
+
+
+logger Starting libdir data sync
 
 cd /var/www/libdir/elasticsearch
 cp /home/datasync/data.tar.gz .
@@ -12,4 +16,6 @@ tar zxvf data.tar.gz
 # so this is enough to restart ES, thanks to 
 # automatic respawn of upstart.
 killall java 
+
+logger Libdir data sync completed
 
