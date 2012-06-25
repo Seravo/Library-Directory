ad15e51403a2d7e951bbf93ca5e74cc951c4bc22
Tero Auvinen <tero@seravo.fi>
2012-06-25 16:36:23 +0300

    Fixes to update-in-production.shFixes to update-in-production.sh

--- update-in-production.sh
+++ update-in-production.sh
@@ -7,12 +7,12 @@ set -e
 # print commands before execution
 set -x
 
-git checkout js/libs/jquery.facetview.js # move away old changes, they would otherwise stop the pull
+git checkout js/libs/jquery.facetview.js js/script.js js/widget.js # move away old changes, they would otherwise stop the pull
 git pull
 
 sed -i "s/localhost/hakemisto.kirjastot.fi/" js/libs/jquery.facetview.js
 sed -i "s/localhost:8080/hakemisto.kirjastot.fi/" js/script.js
-sed -i "s/localhost:8888/hakemisto.kirjastot.fi/" js/script.js
+sed -i "s/localhost:8888/hakemisto.kirjastot.fi:8888/" js/script.js
 sed -i "s/localhost:8080/hakemisto.kirjastot.fi/" js/widget.js
 
 cd css
