5fffc61f6eac8185de6e08bc4c1bbc83607f4345
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2012-09-07 21:09:39 +0300

    Trying to fix ISO scaling bug

--- js/script.js
+++ js/script.js
@@ -545,3 +545,17 @@ function ld_widget_wizard() {
 		}
 	})();
 }
+
+/* 
+Fix IOS scaling bug
+http://stackoverflow.com/questions/5434656/ipad-layout-scales-up-when-rotating-from-portrait-to-landcape
+*/
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+}
