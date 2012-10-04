eb9ee752afff2e0a47743c319533967c9b0ed4f5
Otto Kekäläinen <otto.kekalainen@seravo.fi>
2012-10-04 10:05:45 +0300

    Update_es_data not in use anymore

--- bin/update_es_data.sh
+++ bin/update_es_data.sh
@@ -4,18 +4,18 @@
 # 0 5 * * 1 /var/www/libdir/bin/update_es_data.sh
 
 
-logger Starting libdir data sync
+#logger Starting libdir data sync
 
-cd /var/www/libdir/elasticsearch
-cp /home/datasync/data.tar.gz .
-rm -rf data-old
-mv data data-old
-tar zxvf data.tar.gz
+#cd /var/www/libdir/elasticsearch
+#cp /home/datasync/data.tar.gz .
+#rm -rf data-old
+#mv data data-old
+#tar zxvf data.tar.gz
 # User libdir does not have permission to 
 # stop/start these services, but owns process
 # so this is enough to restart ES, thanks to 
 # automatic respawn of upstart.
-killall java 
+#killall java 
 
-logger Libdir data sync completed
+#logger Libdir data sync completed
 
