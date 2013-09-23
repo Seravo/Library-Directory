2e7d3def7854834e13380da660d51dc7d5ce9b64
Tero Auvinen <tero@seravo.fi>
2013-09-23 14:43:51 +0300

    Typofix.

--- server.js
+++ server.js
@@ -690,7 +690,7 @@ function add_library_metadata(dataobj, callback){
 	} else {
     if (lib.consortium.length >0) {
       var consId = lib.consortium;
-      if (consortiumData[consId].name != "undefined") {
+      if (consortiumData[consId] != "undefined") {
         lib.consortium = consortiumData[consId].name;
         lib.consortium_url = consortiumData[consId].url;
       } else {
