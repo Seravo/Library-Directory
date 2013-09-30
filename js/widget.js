003129d63708968ecab10f29cd17f01b9116516d
Tero Auvinen <tero@seravo.fi>
2013-10-01 00:53:56 +0300

    SSL-fix for widget-javascript.

--- js/widget.js
+++ js/widget.js
@@ -16,7 +16,7 @@ if (window.jQuery === undefined || window.jQuery.fn.jquery !== '1.7.2') {
     var script_tag = document.createElement('script');
     script_tag.setAttribute("type","text/javascript");
     // omit protocol for ssl support
-    script_tag.setAttribute("src","://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+    script_tag.setAttribute("src",proto+"://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
     if (script_tag.readyState) {
       script_tag.onreadystatechange = function () { // for old ie
           if (this.readyState == 'complete' || this.readyState == 'loaded') {
