aa56dde83543b2e5d7263b42f941214e227bf75d
Egor Danchenkov <icepnyx@gmail.com>
2014-02-27 01:13:29 +0200

    Show correct municipality name

--- server.js
+++ server.js
@@ -1138,58 +1138,64 @@ function get_personnel(sstr, ajax,callback) {
     });
     res.on('end', function() {
       dataobj = JSON.parse(data);
-      callback(dataobj.hits.hits);
+      async.mapSeries(dataobj.hits.hits, getOrganizationById, function(err, persons){
+        if(err){
+          rlog(err);
+        } else {
+          callback(persons)
+        }
+      })
     });
   }).on('error', function(e) {
     rlog('Problem with request: ' + e.message);
   });
 }
 
-// function getOrganizationById(person, callback) {
-//   var id = person._source.organisation;
-//   if(id){
+function getOrganizationById(person, callback) {
+  var id = person._source.organisation;
+  if(id){
 
-//     // TODO limit result, only one possible
-//     var query = {
-//       'size': 9999,
-//       'query' : {
-//         "ids" : { 'values' : [id] }
-//       }
-//     };
+    // TODO limit result, only one possible
+    var query = {
+      'size': 9999,
+      'query' : {
+        "ids" : { 'values' : [id] }
+      }
+    };
 
-//     query = JSON.stringify(query);
-//     query = encodeURIComponent(query);
+    query = JSON.stringify(query);
+    query = encodeURIComponent(query);
 
-//     var options = {
-//       host: conf.proxy_config.host,
-//       port: conf.proxy_config.port,
-//       path: '/testink/organisation/_search?source='+query,
-//       method: 'GET'
-//     };
+    var options = {
+      host: conf.proxy_config.host,
+      port: conf.proxy_config.port,
+      path: '/testink/organisation/_search?source='+query,
+      method: 'GET'
+    };
 
-//     var req = http.get(options, function(res) {
-//           res.setEncoding('utf8');
-//           data = '';
-//         res.on('data', function(chunk){
-//           data += chunk;
-//         });
-//         res.on('end', function() {
-//           data = JSON.parse(data);
-//           if(data.hits.hits[0]){
-//             person._source.organisation = data.hits.hits[0]._source;
-//             callback(null, person);
-//           } else {
-//             callback(null, null);
-//           }
-//         });
-//       }).on('error', function(e) {
-//         callback(e);
-//       });
+    var req = http.get(options, function(res) {
+          res.setEncoding('utf8');
+          data = '';
+        res.on('data', function(chunk){
+          data += chunk;
+        });
+        res.on('end', function() {
+          data = JSON.parse(data);
+          if(data.hits.hits[0]){
+            person._source.organisation = data.hits.hits[0]._source;
+            callback(null, person);
+          } else {
+            callback(null, null);
+          }
+        });
+      }).on('error', function(e) {
+        callback(e);
+      });
 
-//   } else {
-//     callback(null, null);
-//   }
-// }
+  } else {
+    callback(null, null);
+  }
+}
 
 // get library by slug
 function get_library_by_name(name, browser_req, callback) {
