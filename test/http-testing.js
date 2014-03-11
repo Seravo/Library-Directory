f954c511e6c4340464fcf460ad76eba3aa611762
Egor Danchenkov <icepnyx@gmail.com>
2014-03-12 01:13:22 +0200

    Add new tests, refacatored code

--- test/http-testing.js
+++ test/http-testing.js
@@ -57,15 +57,157 @@ describe('Libdir tests', function(){
       });
   });
 
-  it('Expect GET "/nonexisitngpage" still returns 200', function(done){
+  it('Expect GET "/about" returns 200', function(done){
     chai.request(app)
-      .get('/nonexisitngpage')
+      .get('/about')
       .res(function (res) {
         chai.expect(res).to.have.status(200);
         chai.expect(res).to.be.html;
         done();
       });
   });
+
+  it('Expect GET "/browse" returns 200', function(done){
+    chai.request(app)
+      .get('/browse')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/contact" returns 200', function(done){
+    chai.request(app)
+      .get('/contact')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/personnel" returns 200', function(done){
+    chai.request(app)
+      .get('/personnel')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/search" returns 200', function(done){
+    chai.request(app)
+      .get('/search')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/feedback-sent" returns 200', function(done){
+    chai.request(app)
+      .get('/feedback-sent')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/pasila" returns 200', function(done){
+    chai.request(app)
+      .get('/pasila')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/en/pasila" returns 200', function(done){
+    chai.request(app)
+      .get('/en/pasila')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  it('Expect GET "/sv/pasila" returns 200', function(done){
+    chai.request(app)
+      .get('/sv/pasila')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+});
+
+describe('Widget tests', function(){
+
+  var LibraryId = 'JBN7mHG2TwCwFBSnYnPEHQ'; // Multilingual Library
+
+  it('Expect widget library search returns json', function(done){
+    var query = {"size":10,"sort":[{"name_fi":{}}],"query":{"query_string":{"fields":["contact.street_address.street_*","name_*"],"query":"*pasila*"}}};
+    chai.request(app)
+      .get(elasticsearch)
+      .req(function (req) {
+        req.send(query);
+      })
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.json;
+        done();
+      });
+  });
+
+  it('Expect GET "/widget" returns 200', function(done){
+    chai.request(app)
+      .get('/widget')
+      .res(function (res) {
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
+        done();
+      });
+  });
+
+  // it('Expect GET "/widget2" returns 200', function(done){
+  //   chai.request(app)
+  //     .get('/widget2')
+  //     .req(function (req) {
+  //       // req.set('jquery:id', LibraryId)
+  //       req.send({sstr:'body'})
+  //     })
+  //     .res(function (res) {
+  //       chai.expect(res).to.have.status(200);
+  //       chai.expect(res).to.be.html;
+  //       done();
+  //     });
+  // });
+
+  // it('Expect GET "/widget3" returns 200', function(done){
+  //   chai.request(app)
+  //     .get('/widget3')
+  //     .req(function (req) {
+  //       req.send({query:{
+  //         id: LibraryId
+  //       }});
+  //     })
+  //     .res(function (res) {
+  //       chai.expect(res).to.have.status(200);
+  //       chai.expect(res).to.be.html;
+  //       done();
+  //     });
+  // });
+
+});
+
+describe('Elasticsearch tests', function(){
 
   it('Expect POST "/personnel-search" returns 200', function(done){
     chai.request(app)
@@ -80,21 +222,19 @@ describe('Libdir tests', function(){
       });
   });
 
-  it('Expect empty POST "/personnel-search" returns 500', function(done){
+  it('Expect empty POST "/personnel-search" returns 200', function(done){
     chai.request(app)
       .post('/personnel-search')
       .req(function (req) {
         req.send();
       })
       .res(function (res) {
-        chai.expect(res).to.have.status(500);
+        chai.expect(res).to.have.status(200);
+        chai.expect(res).to.be.html;
         done();
       });
   });
 
-});
-
-describe('Elasticsearch tests', function(){
   it('Expect main (library) search returns json', function(done){
     var query = {"sort":[{"name_fi":{}}],"query":{"filtered":{"query":{"query_string":{"fields":["name_*","name_short_*","contact.street_address.municipality_*","contact.street_address.post_code*","services.name_*","description_*"],"query":"Pasila*","default_operator":"AND"}},"filter":{"and":[{"terms":{"organisation_type":["branchlibrary","library"]}},{"term":{"meta.document_state":"published"}}]}}},"size":50,"facets":{"contact.street_address.municipality_en":{"terms":{"field":"contact.street_address.municipality_en","order":"term","size":400}},"services.name_en":{"terms":{"field":"services.name_en","order":"term","size":500}},"accessibility.accessible_entry":{"terms":{"field":"accessibility.accessible_entry"}},"accessibility.accessible_parking":{"terms":{"field":"accessibility.accessible_parking"}},"accessibility.accessible_toilet":{"terms":{"field":"accessibility.accessible_toilet"}},"accessibility.induction_loop":{"terms":{"field":"accessibility.induction_loop"}},"accessibility.large_typeface_collection":{"terms":{"field":"accessibility.large_typeface_collection"}},"accessibility.lift":{"terms":{"field":"accessibility.lift"}},"accessibility.extraaccessibilityinfo":{"terms":{"field":"accessibility.extraaccessibilityinfo"}},"consortium":{"terms":{"field":"consortium","size":100,"order":"term"}},"provincial_area":{"terms":{"field":"provincial_area","size":100,"order":"term"}},"organisation_type":{"terms":{"field":"organisation_type"}},"branch_type":{"terms":{"field":"branch_type"}}}};
     chai.request(app)
@@ -109,7 +249,7 @@ describe('Elasticsearch tests', function(){
       });
   });
 
-  it('Expect widget library search returns json', function(done){
+  it('Expect personnel search returns json', function(done){
     var query = {"size":10,"sort":[{"name_fi":{}}],"query":{"query_string":{"fields":["contact.street_address.street_*","name_*"],"query":"*pasila*"}}};
     chai.request(app)
       .get(elasticsearch)
